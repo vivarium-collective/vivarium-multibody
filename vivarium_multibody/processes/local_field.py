7e19897644f44cdbd7ec426186ed5f55d705c110
Eran Agmon <agmon.eran@gmail.com>
2021-03-11 12:06:25 -0800

    update local_field

--- vivarium_multibody/processes/local_field.py
+++ vivarium_multibody/processes/local_field.py
@@ -3,38 +3,47 @@ import numpy as np
 from vivarium.core.process import Deriver
 from vivarium.library.units import units, remove_units
 
-from vivarium_multibody.library.lattice_utils import (
+from vivarium_cobra.library.lattice_utils import (
     get_bin_site,
     get_bin_volume,
     count_to_concentration,
 )
 
-CONCENTRATION_UNIT = 1
-LENGTH_UNIT = 1  # units.um
-
 
 class LocalField(Deriver):
 
     name = 'local_field'
-    defaults = {}
+    defaults = {
+        'initial_external': {},
+        'nonspatial': False,
+        'bin_volume': 1e-6 * units.L,
+    }
 
     def __init__(self, parameters=None):
         super(LocalField, self).__init__(parameters)
+        self.nonspatial = self.parameters['nonspatial']
+        self.bin_volume = self.parameters['bin_volume']
+
+
+    def initial_state(self, config=None):
+        return {
+            'fields': self.parameters['initial_external']
+        }
+
 
     def ports_schema(self):
-         return {
+        return {
             'exchanges': {
                 '*': {
                     '_default': 0,  # counts!
                 }
             },
             'location': {
-                '_default': [0.5 * LENGTH_UNIT, 0.5 * LENGTH_UNIT]
+                '_default': [0.5, 0.5]
             },
             'fields': {
                 '*': {
-                    '_default': np.ones(1),
-                    '_updater': 'accumulate',
+                    '_default': np.ones(1) if not self.nonspatial else 1.0,
                 }
             },
             'dimensions': {
@@ -58,23 +67,38 @@ class LocalField(Deriver):
         depth = states['dimensions']['depth']
         exchanges = states['exchanges']
 
-        # get bin
-        bin_site = get_bin_site(location, n_bins, bounds)
-        bin_volume = get_bin_volume(n_bins, bounds, depth) * units.L
+        # get bin volume
+        if self.nonspatial:
+            bin_volume = self.bin_volume
+        else:
+            bin_site = get_bin_site(location, n_bins, bounds)
+            bin_volume = get_bin_volume(n_bins, bounds, depth) * units.L
 
         # apply exchanges
         delta_fields = {}
         reset_exchanges = {}
         for mol_id, value in exchanges.items():
-            delta_fields[mol_id] = np.zeros(
-                (n_bins[0], n_bins[1]), dtype=np.float64)
+
+            # delta concentration
             exchange = value * units.count
-            concentration = count_to_concentration(exchange, bin_volume)
-            delta_fields[mol_id][bin_site[0], bin_site[1]] += concentration.to(
+            concentration = count_to_concentration(exchange, bin_volume).to(
                 units.mmol / units.L).magnitude
+
+            if self.nonspatial:
+                delta_fields[mol_id] = {
+                    '_value': concentration,
+                    '_updater': 'nonnegative_accumulate'}
+            else:
+                delta_field = np.zeros((n_bins[0], n_bins[1]), dtype=np.float64)
+                delta_field[bin_site[0], bin_site[1]] += concentration
+                delta_fields[mol_id] = {
+                    '_value': delta_field,
+                    '_updater': 'nonnegative_accumulate'}
+
+            # reset the exchange value
             reset_exchanges[mol_id] = {
-                '_value': 0,
-                '_updater': 'set'}
+                '_value': -value,
+                '_updater': 'accumulate'}
 
         return {
             'exchanges': reset_exchanges,
