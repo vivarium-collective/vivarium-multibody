8a47815b266614369674297481d0737cf715c6ec
Eran Agmon <agmon.eran@gmail.com>
2021-04-22 16:18:55 -0700

    v0.0.10

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", 'r') as readme:
 
 setup(
     name='vivarium-multibody',
-    version='0.0.9',
+    version='0.0.10',
     packages=[
         'vivarium_multibody',
         'vivarium_multibody.plots',
